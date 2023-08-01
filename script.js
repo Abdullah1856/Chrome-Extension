199b638ca8d29ade5939bdf476da44f04a53e7b3
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-08-01 11:37:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function btn() {
 }
 */
 
+myLeads = []
+inputEl = document.getElementById('input-el')
+
 /* Event Listner */
 let inBtn = document.getElementById("button-el")
-
 inBtn.addEventListener("click", function () {
     console.log('This is it!');
 })
