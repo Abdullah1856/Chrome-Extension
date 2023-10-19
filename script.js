5e8f03bbedefed7afe30c85280dd3428d5e8383f
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-10-19 13:53:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 const btnClicked = document.getElementById('button-el')
-let value = 'abdullah.com'
 
 btnClicked.addEventListener("click", function () {
     myLeads.push('abdullah.com')
