0bdcc2cb8f9da1e4002b17e0cccc290289d42fe9
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-11-26 06:04:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-let myLeads = []
+// let myLeads = []
+// // const inputEl = document.getElementById('input-el')
+// const btnClicked = document.getElementById('button-el')
+
+
+// // btnClicked.addEventListener('click', function () {
+// //     myLeads.push("abdullah.com")
+// //     console.log(myLeads);
+// // }
+// // )
+
+// btnClicked.addEventListener('click', function () {
+//     getValue = document.getElementById('input-el').value;
+//     myLeads.push(getValue);
+//     console.log(myLeads);
+//     return false;
+// }
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let myLeads = ['hi', 'bye', 'lie']
 const inputEl = document.getElementById('input-el')
 const btnClicked = document.getElementById('button-el')
 
@@ -17,8 +59,12 @@ const btnClicked = document.getElementById('button-el')
 // }
 // )
 
-buttonEl.addEventListener("click", function() {
+btnClicked.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     console.log(myLeads);
 }
 )
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i]);
+}
