4a0e95918fc2a96127a3d681249a0b2261538297
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-11-26 06:06:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,6 @@
-// let myLeads = []
-// // const inputEl = document.getElementById('input-el')
-// const btnClicked = document.getElementById('button-el')
-
-
-// // btnClicked.addEventListener('click', function () {
-// //     myLeads.push("abdullah.com")
-// //     console.log(myLeads);
-// // }
-// // )
-
-// btnClicked.addEventListener('click', function () {
-//     getValue = document.getElementById('input-el').value;
-//     myLeads.push(getValue);
-//     console.log(myLeads);
-//     return false;
-// }
-// )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let myLeads = ['hi', 'bye', 'lie']
 const inputEl = document.getElementById('input-el')
 const btnClicked = document.getElementById('button-el')
-
-
-// btnClicked.addEventListener('click', function () {
-//     myLeads.push("abdullah.com")
-//     console.log(myLeads);
-// }
-// )
-
-// btnClicked.addEventListener('click', function () {
-//     getValue = document.getElementById('input-el').value;
-//     myLeads.push(getValue);
-//     console.log(myLeads);
-//     return false;
-// }
-// )
 
 btnClicked.addEventListener("click", function () {
     myLeads.push(inputEl.value)
