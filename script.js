6b015879040f7f5fc4bba23fbd3259932a19a5c1
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-11-28 11:00:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ btnClicked.addEventListener("click", function () {
 function renderLeads() {
     let listItems = ''
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li> <a href='${myLeads[i]}' target='_blank'> ${myLeads[i]} </a> </li>`
+        listItems += `
+        <li>
+        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a> 
+        </li>
+        `
     }
 
     ulEl.innerHTML = listItems
