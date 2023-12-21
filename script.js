07693b8c67bf3d3549afb295da0bda1c405efdf0
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-12-21 09:44:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ const tabs = [
 ]
 
 tabBtn.addEventListener('click', function () {
-    // console.log(tabs[0].url)
     myLeads.push(tabs[0].url)
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
     render(myLeads)
