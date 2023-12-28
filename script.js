e8688a71b1383d1b9cebb1e2d36b5c0368664375
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-12-28 11:29:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,16 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-const tabs = [
-    { url: "github.com" }
-]
-
 tabBtn.addEventListener('click', function () {
-    // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    // });
-
-    myLeads.push(tabs[0].url)
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    render(myLeads)
-}
-)
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem('myLeads', JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItems = ''
@@ -43,8 +40,7 @@ deleteBtn.addEventListener('dblclick', function () {
     localStorage.clear()
     ulEl.innerHTML = ''
     myLeads = ''
-}
-)
+})
 
 btnClicked.addEventListener("click", function () {
     myLeads.push(inputEl.value)
